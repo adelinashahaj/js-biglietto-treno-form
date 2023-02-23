40a22aafa697381141e488263ed2c7902d03e9aa
adelinashahaj <adelinarucaj@icloud.com>
2023-02-23 17:55:26 +0100

    esercizio concluso

--- js/main.js
+++ js/main.js
@@ -17,28 +17,31 @@ domButton.addEventListener('click',
 
 
 
-let prezzo = kilometri * 0.21;
-let sconto = ""; 
+         let prezzo = kilometri * 0.21;
+        let sconto = ""; 
 
-if (casellaSelezioneDom.value == "sotto18"){
-    prezzo = (prezzo / 100 * 80).toFixed(2);
-    sconto = "Hai lo sconto di 20% <br> ";  
-    document.getElementById("offerta").innerHTML = "Offerta <br> <br>  Minorenne";  
+        if (casellaSelezioneDom.value == "sotto18"){
+         prezzo = (prezzo / 100 * 80).toFixed(2);
+        sconto = "Hai lo sconto di 20% <br> ";  
+        document.getElementById("offerta").innerHTML = "Offerta <br> <br>  Minorenne"; 
    
-} else if (casellaSelezioneDom.value =="sopra65"){
-    prezzo = (prezzo / 100 * 60).toFixed(2);
-    sconto = "Hai lo sconto di 40% <br> "; 
-    document.getElementById("offerta").innerHTML = "Offerta <br> <br> Senior (Over 65)";   
+   
+         } else if (casellaSelezioneDom.value =="sopra65"){
+         prezzo = (prezzo / 100 * 60).toFixed(2);
+         sconto = "Hai lo sconto di 40% <br> "; 
+          document.getElementById("offerta").innerHTML = "Offerta <br> <br> Senior (Over 65)";   
    
   
-}else {
-    prezzo = prezzo.toFixed(2);
-    document.getElementById("offerta").innerHTML = "Nessuna <br> offerta";
+         }else {
+         prezzo = prezzo.toFixed(2);
+          document.getElementById("offerta").innerHTML = "Nessuna <br> offerta";
 
-}
+        }
 
-console.log(prezzo);
-console.log(kilometri);
+         console.log(prezzo);
+        document.getElementById("costo").innerHTML = `prezzo biglietto <br> <br> ${prezzo} euro`;
+   
+        console.log(kilometri);
 
     }
    
@@ -46,18 +49,9 @@ console.log(kilometri);
 );
 document.getElementById("carrozza").innerHTML = "Carrozza <br><br>"  + Math.ceil((Math.random() * 15) + 1);
 
-// Codice CP
+
 document.getElementById("codice").innerHTML ="Codice CP <br><br>" + Math.ceil(Math.random() * (100000 - 90000) + 90000);
 
 
-/*
-const domReset = document.querySelector('#resettaForm');
 
-domReset.addEventListener('click', 
-    function() {
-        const messaggioDom = document.getElementById('etaViaggiatore');
-        messaggioDom.value = "";
-    }
-);
-*/
 
