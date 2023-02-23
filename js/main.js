170c813f4bbf7d98cf022fde68fdb787edf40f9f
adelinashahaj <adelinarucaj@icloud.com>
2023-02-23 17:17:08 +0100

    manca solo da stampare il prezzo

--- js/main.js
+++ js/main.js
@@ -1,43 +1,18 @@
 
-/*
-let etaViaggiatore = "";
-let kilometri = "";
-
-
-let prezzo = kilometri * 0.21;
-let sconto = ""; 
-
-if (etaViaggiatore < 18){
-    prezzo = (prezzo / 100 * 80).toFixed(2);
-    sconto = "Hai lo sconto di 20% <br> ";    
-   
-} else if (etaViaggiatore > 65){
-    prezzo = (prezzo / 100 * 60).toFixed(2);
-    sconto = "Hai lo sconto di 40% <br> ";    
-   
-  
-}else {
-    prezzo = prezzo.toFixed(2);
-
-}
-
-document.getElementById("biglietto").innerHTML = sconto + "Il biglietto costa: "  + prezzo + "€";
-*/
-
-
-
-
 const domButton = document.querySelector('#leggiForm');
 
 domButton.addEventListener('click', 
     function () {
         const messaggioDom = document.getElementById('nomeViaggiatore');
         const testoDelMessaggio = messaggioDom.value;
+        document.getElementById("nome").innerHTML ="Nome passeggero <br> <br> " + testoDelMessaggio;
         const kilometriDom = document.getElementById('kilometri');
         const kilometri = kilometriDom.value;
         console.log(kilometri);
+        
         const casellaSelezioneDom = document.getElementById('casellaSelezione');
         console.log(casellaSelezioneDom.value);
+        
         let nomeViaggiatore = "";
 
 
@@ -47,15 +22,18 @@ let sconto = "";
 
 if (casellaSelezioneDom.value == "sotto18"){
     prezzo = (prezzo / 100 * 80).toFixed(2);
-    sconto = "Hai lo sconto di 20% <br> ";    
+    sconto = "Hai lo sconto di 20% <br> ";  
+    document.getElementById("offerta").innerHTML = "Offerta <br> <br>  Minorenne";  
    
 } else if (casellaSelezioneDom.value =="sopra65"){
     prezzo = (prezzo / 100 * 60).toFixed(2);
-    sconto = "Hai lo sconto di 40% <br> ";    
+    sconto = "Hai lo sconto di 40% <br> "; 
+    document.getElementById("offerta").innerHTML = "Offerta <br> <br> Senior (Over 65)";   
    
   
 }else {
     prezzo = prezzo.toFixed(2);
+    document.getElementById("offerta").innerHTML = "Nessuna <br> offerta";
 
 }
 
@@ -63,8 +41,14 @@ console.log(prezzo);
 console.log(kilometri);
 
     }
+   
 
 );
+document.getElementById("carrozza").innerHTML = "Carrozza <br><br>"  + Math.ceil((Math.random() * 15) + 1);
+
+// Codice CP
+document.getElementById("codice").innerHTML ="Codice CP <br><br>" + Math.ceil(Math.random() * (100000 - 90000) + 90000);
+
 
 /*
 const domReset = document.querySelector('#resettaForm');
@@ -77,11 +61,3 @@ domReset.addEventListener('click',
 );
 */
 
-const domElement = document.querySelector('#generatoreCasuale');
-
-domElement.addEventListener('click', 
-    function() {
-        const numeroCasuale = Math.floor((Math.random() * 901)) + 100;
-       console.log(numeroCasuale);
-    }
-);
